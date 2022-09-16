aa5f9199e75eae918c82d0c4742d3bc4c75b5337
ERlKS0N <90794114+ERlKS0N@users.noreply.github.com>
2022-09-16 08:14:00 -0500

    commit push
    You gotta do Layout and gravity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void screen2(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        Bundle b = ActivityOptions.makeSceneTransitionAnimation(this).toBundle();
+        startActivity(i,b);
+    }
+    public void screen3(View view){
+        Intent i = new Intent(this, MainActivity3.class);
+        Bundle b = ActivityOptions.makeSceneTransitionAnimation(this).toBundle();
+        startActivity(i,b);
     }
 }
