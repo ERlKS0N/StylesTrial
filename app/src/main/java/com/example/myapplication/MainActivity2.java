560b0c822d746a7e66c6f228840470daa01279e0
ERlKS0N <90794114+ERlKS0N@users.noreply.github.com>
2022-09-20 09:29:31 -0500

    commit push
    layouts and make sure to do themes.

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +16,19 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+    public void changeGravity(View view){
+        TextView textArea = findViewById(R.id.gravityChamber);
+        ArrayList<Integer> gravities = new ArrayList<Integer>();
+        gravities.add(Gravity.END);
+        gravities.add(Gravity.FILL);
+        gravities.add(Gravity.CENTER);
+        gravities.add(Gravity.TOP);
+        gravities.add(Gravity.BOTTOM);
+        gravities.add(Gravity.CENTER_HORIZONTAL);
+        gravities.add(Gravity.CENTER_VERTICAL);
+        int gravity = (int)(Math.random() * 7);
+
+        textArea.setGravity(gravities.get(gravity));
+    }
+
 }
